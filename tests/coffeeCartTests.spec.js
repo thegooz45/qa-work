eca172e8eefd81a375ffa044496ae6d95abf0933
bguzman4590 <briguz4590@gmail.com>
2025-01-24 12:29:17 -0500

    negative test

--- tests/coffeeCartTests.spec.js
+++ tests/coffeeCartTests.spec.js
@@ -13,20 +13,33 @@ import { test, expect } from '@playwright/test'
 // before each test, nav to the site
 test.beforeEach(async ({ page }) => {
     await page.goto('https://coffee-cart.app/');
+
+    // verifying we're on the page first
+    await expect(page.getByText('Flat White')).toBeVisible();
   });
 
 
-// 1st positive test!
-test.describe('Selecting an option', () => {
+test.describe('Positive test', () => {
    test('allow me to add an option to cart', async ({ page }) => {
-        // verifying we're on the page first (could put this in the beforeEach)
-        await expect(page.getByText('Flat White')).toBeVisible();
-
         // add an option to cart
         await page.getByLabel('Mocha').click();
 
         // verify it has been added to cart via updated price
         await expect(page.getByText('Total: $8.00')).toBeVisible();
+    });
+});
+
+test.describe('Negative test', () => {
+    test('removing an order', async ({ page }) => {
+        // add an option to cart and verifying it's added to cart
+        await page.getByLabel('Mocha').click();
+        await expect(page.getByText('Total: $8.00')).toBeVisible();
+
+        // removing it via '-' button and verifying the order has been removed
+        await page.getByText('Total: $8.00').hover();
+        await page.getByText('-').click({force: true});
+      
+        await expect(page.getByText('Total: $0.00')).toBeVisible();
     });
 });
 
